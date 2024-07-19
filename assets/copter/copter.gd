ebf8a1d832828f65baebad6a10ec6d5aef989c80
divyendu <divyendua@gmail.com>
2024-07-18 22:37:27 -0400

    implement ui

--- assets/copter/copter.gd
+++ assets/copter/copter.gd
@@ -7,9 +7,10 @@ func _ready():
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
+func _process(_delta):
+	if position.y > 800 or position.y < -800:
+		get_tree().change_scene_to_file("res://scenes/game_over.tscn")
 
 func _input(event):
 	if event.is_pressed():
-		self.linear_velocity = Vector2(0, -600)
+		self.linear_velocity = Vector2(0, -800)
---
+++ assets/ui/Home/home.gd
@@ -0,0 +1,9 @@
+extends TextureButton
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	connect("pressed", Callable(self, "_button_pressed"))
+
+func _button_pressed():
+	get_tree().change_scene_to_file("res://scenes/main_menu.tscn")
