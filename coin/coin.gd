c435df19b75544ab7c390d67f63003f37730906b
divyendu <divyendua@gmail.com>
2025-01-02 23:16:09 -0800

    coin collect audio

--- coin/coin.gd
+++ coin/coin.gd
@@ -1,8 +1,14 @@
 extends Area2D
 
 func _on_body_entered(_body):
+	$collect_sound.play()
+	
 	Global.current_score += 1
 	if Global.current_score >= 20:
 		Global.SPEED = -700
 
+	$Coin.queue_free()
+	$collision.queue_free()
+
+	await $collect_sound.finished
 	queue_free()
